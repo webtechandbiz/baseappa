2dbd73932eff34352169d5f3a9ca719be9211707
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-11-29 19:20:07 +0100

    Update homegrid.dart

--- src/screens/homegrid.dart
+++ src/screens/homegrid.dart
@@ -1,151 +1,132 @@
 import 'dart:convert';
 
 import 'package:baseappahome/main.dart';
+import 'package:baseappahome/src/config/app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:baseappahome/src/auth/http/login_http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:baseappahome/src/design/design_objects.dart';
 
 //#https://flutter.dev/docs/cookbook/navigation/navigation-basics
-class HomeRoute extends StatelessWidget {
+class HomeRoute extends StatefulWidget {
   const HomeRoute({Key? key}) : super(key: key);
 
+  @override
+  State<HomeRoute> createState() => _HomeRouteState();
+}
+
+class _HomeRouteState extends State<HomeRoute> {
   Future<void> setAuthenticationToken(String authenticationtoken) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString("authenticationtoken", authenticationtoken);
   }
+  DesignObjects designobjects = new DesignObjects();
+  Future<String> _getAuthenticationToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('authenticationtoken') ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Category> _categoryList = [];
+    List<ContentBox> _data1List = [];
+    List<ContentBox> _data2List = [];
     String imgsrc = '';
     final homeArgs = ModalRoute.of(context)!.settings.arguments as ScreenArgumentsFutureLoginFormDataParameters;
-
+    Future<String> _authtokenlogin_saved = _getAuthenticationToken();
     late Future<Homedata> futureHomedata = homeArgs.futureLoginFormData;
 
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue, //#change colore (del titolo)
-          title: const Text('Home'), //#change titolo sopra campi di login
-        ),
-        body: FutureBuilder<Homedata>(
-          future: futureHomedata,
-          builder: (context, snapshot) {
+      drawer: designobjects.get_drawer(context, _authtokenlogin_saved), //# change per il menu
+      appBar: AppBar(
+        backgroundColor: Colors.blue, //#change colore (del titolo)
+        title: const Text('Home'), //#change titolo sopra campi di login
+      ),
+      body: FutureBuilder<Homedata>(
+        future: futureHomedata,
+        builder: (context, snapshot) {
 
-            if (snapshot.hasData) {
-              String authenticationtoken = snapshot.data!.authenticationToken;
-              setAuthenticationToken(authenticationtoken);
+          if (snapshot.hasData) {
+            String authenticationtoken = snapshot.data!.authenticationToken;
+            setAuthenticationToken(authenticationtoken);
 
-              for (var item in snapshot.data!.spuntieducativi!){
-                if(item['imgurl'] != false && item['imgurl'] != ''){
-                  imgsrc = item['imgurl'];
-                }else{
-                  imgsrc = ''; //#change url immagina di default se l'immagine manca
-                }
+            //#section 1
+            for (var item in snapshot.data!.spuntieducativi!){
+              if(item['imgurl'] != false && item['imgurl'] != ''){
+                imgsrc = item['imgurl'];
+              }else{
+                imgsrc = ''; //#change url immagina di default se l'immagine manca
+              }
 
-                _categoryList.add(
-                    Category(
+              _data1List.add(
+                  ContentBox(
+                    authenticationToken: authenticationtoken,
+                    IDitem: item['_ID'],
+                    item: item,
+                    imagePath: imgsrc,
+                    title: item['titlelenght'],
+                    excerpt: item['titlelenght']
+                  )
+              );
+            }
+            Section section1 = new Section(sectionTitle: 'Spunti educativi 1', dataList: _data1List);
+
+            //#section 2
+            for (var item in snapshot.data!.spuntieducativi!){
+              if(item['imgurl'] != false && item['imgurl'] != ''){
+                imgsrc = item['imgurl'];
+              }else{
+                imgsrc = ''; //#change url immagina di default se l'immagine manca
+              }
+
+              _data2List.add(
+                  ContentBox(
                       authenticationToken: authenticationtoken,
                       IDitem: item['_ID'],
                       item: item,
                       imagePath: imgsrc,
-                      title: item['titlelenght']
-                    )
-
-                );
-              }
-
-              return GridView.count(
-                crossAxisCount: 2, //#change numero colonne
-                children: _categoryList.map((value) {
-                  return Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.all(8), //#change spazio fra un box e l'altro
-                    decoration:
-                    BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                    ), //#change colore del bordo del singolo box
-                    child: Row(
-                      children: [
-                        Expanded(
-                          /*1*/
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              /*2*/
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context,
-                                        '/scheda',
-                                        arguments: ScreenArgumentsSchedaParameters(value.authenticationToken, value.IDitem)
-                                    );
-                                  },
-                                  child: ClipRRect(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                                image: NetworkImage("${value.imagePath}"),
-                                                fit: BoxFit.cover //#change zoomma l'immagine riempiendo la riga (quindi le parti dx e sx dell'immagine non si vedrà)
-                                              //#fit: BoxFit.fill //#change stretcha l'immagine
-                                            )
-                                        ),
-                                      )
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                //padding: const EdgeInsets.all(0), //#change spazio interno al box
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8),
-                                  child: Text( "${value.title}",
-                                    /*#change modifica lo stile del box
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),*/
-                                  ),
-                                ),
-                              ),
-
-                              /*#change: testo per sottotitolo
-                              Text(
-                                'sottotitolo',
-                                style: TextStyle(
-                                  color: Colors.grey[500],
-                                ),
-                              ),*/
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
+                      title: item['titlelenght'],
+                      excerpt: item['titlelenght']
+                  )
               );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
             }
+            Section section2 = new Section(sectionTitle: 'Spunti educativi 2', dataList: _data2List);
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
-        )
+            Sections sections = new Sections(section1: section1, section2: section2);
+            //DesignObjects designobjects = new DesignObjects();
+            List <Widget> homeSections = [
+              designobjects.horizontalScrollSection(sections.section1),
+              Text('asdasdasdasd'),
+              designobjects.verticalScrollSection(sections.section2),
+              designobjects.verticalSection(sections.section2)
+            ];
+
+            return Scaffold(
+              body: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Container( //# contenitore di tutta la pagina
+                        //# l'altezza totale dovrebbe essere la somma di tutte le sezioni + uno spazio vuoto
+                        // oppure se le sezioni sono dinamiche, l'altezza non viene impostata
+                        // e per avere uno spazio vuoto in fondo basta aggiungere un divider o un Widget vuoto
+                        //height: MediaQuery.of(context).size.height + 500,
+                        child: Column( //# una colonna per contenuti dinamici e statici
+                          children: homeSections,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      )
     );
   }
-}
-
-class Category {
-  Category({
-    required this.authenticationToken,
-    required this.IDitem,
-    required this.item,
-    this.title = '',
-    this.imagePath = ''
-  });
-
-  int IDitem;
-  Object item;
-  String title;
-  String imagePath;
-  String authenticationToken;
 }
