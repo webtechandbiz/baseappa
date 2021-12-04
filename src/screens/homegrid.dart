105bfe3388cbd6004fa620275c0bd144842cdb27
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-12-04 19:26:46 +0100

    Update homegrid.dart

--- src/screens/homegrid.dart
+++ src/screens/homegrid.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:baseappahome/main.dart';
+import 'package:baseappahome/src/config/app_cacher.dart';
 import 'package:baseappahome/src/config/app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:baseappahome/src/auth/http/login_http.dart';
@@ -16,6 +17,19 @@ class HomeRoute extends StatefulWidget {
 }
 
 class _HomeRouteState extends State<HomeRoute> {
+  ManageStorage _cs = ManageStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    _cs.readCache().then((String value) {
+      setState(() {
+        print('*****CACHED-DATA ****');
+        print(value);
+      });
+    });
+  }
+
   Future<void> setAuthenticationToken(String authenticationtoken) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString("authenticationtoken", authenticationtoken);
@@ -41,6 +55,181 @@ class _HomeRouteState extends State<HomeRoute> {
         backgroundColor: Colors.blue, //#change colore (del titolo)
         title: const Text('Home'), //#change titolo sopra campi di login
       ),
+
+      body: Center(
+        /*
+        child: _widgetOptions.elementAt(_BottomBarSelectedIndex),
+        */
+        child: FutureBuilder<Homedata>(
+          future: futureHomedata,
+          builder: (context, snapshot) {
+
+            if (snapshot.hasData) {
+              String authenticationtoken = snapshot.data!.authenticationToken;
+              setAuthenticationToken(authenticationtoken);
+
+              //_cs.writeCache(snapshot.data!.toString());
+
+              //#section 1
+              for (var item in snapshot.data!.spuntieducativi!){
+                if(item['imgurl'] != false && item['imgurl'] != ''){
+                  imgsrc = item['imgurl'];
+                }else{
+                  imgsrc = ''; //#change url immagina di default se l'immagine manca
+                }
+
+                _data1List.add(
+                    ContentBox(
+                        authenticationToken: authenticationtoken,
+                        IDitem: item['_ID'],
+                        item: item,
+                        imagePath: imgsrc,
+                        title: item['titlelenght'],
+                        excerpt: item['titlelenght']
+                    )
+                );
+              }
+              Section section1 = new Section(sectionTitle: 'Spunti educativi 1', dataList: _data1List);
+
+              //#section 2
+              for (var item in snapshot.data!.spuntieducativi!){
+                if(item['imgurl'] != false && item['imgurl'] != ''){
+                  imgsrc = item['imgurl'];
+                }else{
+                  imgsrc = ''; //#change url immagina di default se l'immagine manca
+                }
+
+                _data2List.add(
+                    ContentBox(
+                        authenticationToken: authenticationtoken,
+                        IDitem: item['_ID'],
+                        item: item,
+                        imagePath: imgsrc,
+                        title: item['titlelenght'],
+                        excerpt: item['titlelenght']
+                    )
+                );
+              }
+              Section section2 = new Section(sectionTitle: 'Spunti educativi 2', dataList: _data2List);
+
+              Sections sections = new Sections(section1: section1, section2: section2);
+              //DesignObjects designobjects = new DesignObjects();
+              List <Widget> homeSections = [
+                designobjects.horizontalScrollSection(sections.section1),
+                Text('asdasdasdasd'),
+                designobjects.verticalScrollSection(sections.section2),
+                designobjects.verticalSection(sections.section2),
+                Column(
+                  children: [
+                    TextButton(
+                      child: const Text(
+                        'Button1',
+                        style: TextStyle(
+                          //#change fontWeight: FontWeight.bold,
+                          color: Colors.white, //#change colore (del bottone per accedere)
+                        ),
+                      ), //#change bottone per accedere
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          minimumSize: Size(MediaQuery.of(context).size.width/2,40)
+                      ),
+                      onPressed: () async {
+
+                      },
+                    ),
+                  ],
+                ), //# 1 column, 1 button
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        TextButton(
+                          child: const Text(
+                            'Button1',
+                            style: TextStyle(
+                              //#change fontWeight: FontWeight.bold,
+                              color: Colors.white, //#change colore (del bottone per accedere)
+                            ),
+                          ), //#change bottone per accedere
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              minimumSize: Size(MediaQuery.of(context).size.width/2,40)
+                          ),
+                          onPressed: () async {
+
+                          },
+                        ),
+                        TextButton(
+                          child: const Text(
+                            'Button2',
+                            style: TextStyle(
+                              //#change fontWeight: FontWeight.bold,
+                              color: Colors.white, //#change colore (del bottone per accedere)
+                            ),
+                          ), //#change bottone per accedere
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              minimumSize: Size(MediaQuery.of(context).size.width/2,40)
+                          ),
+                          onPressed: () async {
+
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ), //# 2 columns, 2 buttons
+                Column(
+                  children: [
+                    TextButton(
+                      child: const Text(
+                        'Button3',
+                        style: TextStyle(
+                          //#change fontWeight: FontWeight.bold,
+                          color: Colors.white, //#change colore (del bottone per accedere)
+                        ),
+                      ), //#change bottone per accedere
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          minimumSize: Size(MediaQuery.of(context).size.width,40)
+                      ),
+                      onPressed: () async {
+
+                      },
+                    ),
+                  ],
+                ), //# 1 column, 1 button
+              ];
+
+              return Scaffold(
+                body: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Container( //# contenitore di tutta la pagina
+                          //# l'altezza totale dovrebbe essere la somma di tutte le sezioni + uno spazio vuoto
+                          // oppure se le sezioni sono dinamiche, l'altezza non viene impostata
+                          // e per avere uno spazio vuoto in fondo basta aggiungere un divider o un Widget vuoto
+                          //height: MediaQuery.of(context).size.height + 500,
+                          child: Column( //# una colonna per contenuti dinamici e statici
+                            children: homeSections,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+      /*
       body: FutureBuilder<Homedata>(
         future: futureHomedata,
         builder: (context, snapshot) {
@@ -206,7 +395,54 @@ class _HomeRouteState extends State<HomeRoute> {
           // By default, show a loading spinner.
           return const CircularProgressIndicator();
         },
-      )
+      ),
+      */
+      //# https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Logout',
+          ),
+        ],
+        currentIndex: _BottomBarSelectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onBottomBarItemTapped,
+      ),
     );
   }
+
+
+  void _onBottomBarItemTapped(int index) {
+    setState(() {
+      _BottomBarSelectedIndex = index;
+    });
+  }
+
+  int _BottomBarSelectedIndex = 0;
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
 }
